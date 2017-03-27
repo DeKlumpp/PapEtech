1b47b0c8f6159ca79d27c6de757fb58be9564311
Caio Teixeira <caio.teixeira@zbr7.com.br>
2017-03-27 16:47:53 -0300

    cadastro para login

--- app/src/main/java/com/example/vanessa/e_vagas/CadastroActivity.java
+++ app/src/main/java/com/example/vanessa/e_vagas/CadastroActivity.java
@@ -1,7 +1,10 @@
 package com.example.vanessa.e_vagas;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class CadastroActivity extends AppCompatActivity{
 
@@ -10,4 +13,13 @@ public class CadastroActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cadastro);
     }
+
+   public void mensagenExibir(View view){
+        Context context = this;
+        Toast toast = Toast.makeText(context, "Cadastrado com Sucesso", Toast.LENGTH_LONG);
+        toast.show();
+       setContentView(R.layout.login);
+
+};
 }
+
